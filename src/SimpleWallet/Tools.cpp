f4648adf0e183b39c8eed86d7bfd98bdeb291eb9
Davor Suvak <worktipscoin@gmail.com>
2018-10-02 11:41:54 +0200

    Format fix
    Proper formatting of cents in simplewallet.

--- src/SimpleWallet/Tools.cpp
+++ src/SimpleWallet/Tools.cpp
@@ -24,7 +24,7 @@ std::string formatAmount(uint64_t amount)
 {
 
     uint64_t dollars = amount / 100000000;
-    uint64_t cents = amount % 100;
+    uint64_t cents = amount % 100000000;
 
     return formatDollars(dollars) + "." + formatCents(cents) + " WTIP";
 }
